505c0cfc0f208f3a533d9084e17714c94317763e
NekoSakuraLucia <mokurolucia@gmail.com>
2025-03-02 23:28:21 +0700

    เพิ่มการตั้งค่า manualChunks ใน vite config

--- quote-thai/vite.config.js
+++ quote-thai/vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        let chunkName = null;
+
+                        if (id.includes('framer-motion')) {
+                            chunkName = 'motion-vendor';
+                        }
+
+                        if (id.includes('react') || id.includes('react-dom')) {
+                            chunkName = 'react-vendor';
+                        }
+
+                        return chunkName;
+                    }
+                },
+            },
+        },
+    },
+});
